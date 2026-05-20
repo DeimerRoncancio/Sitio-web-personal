d52b5368a433c5b8d5502d6168dc85892f9d61ee
Deimer Roncancio <deimerroncancio@gmail.com>
2026-05-20 18:53:17 -0500

    style: pequeño ajuste de responsive en barra de navegación aplicado

--- src/dashboard/components/items-draggables.jsx
+++ src/dashboard/components/items-draggables.jsx
@@ -6,11 +6,11 @@ export default function ItemsDraggables({ icon, name, isDragging, isShadow = fal
   const tailwindClasses = [
     isHighlighted ? '!border-cyan-500 !from-[#3b4355] !to-[#00586e] !opacity-100' : 'opacity-50 hover:opacity-100 hover:border-[#acacac]',
     (isDragging && isShadow) ? 'scale-95' : '',
-    (isDragging && !isShadow) ? 'scale-110 !h-[95px]' : '',
+    (isDragging && !isShadow) ? 'scale-110 !h-[55px] 2sx:!h-[65px] sm:!h-[95px]' : '',
   ].filter(Boolean).join(' ');
 
   return (
-    <div className={`draggable ${tailwindClasses} flex w-[55px] h-[50px] 2xs:w-[65px] 2xs:h-[60px
+    <div className={`draggable ${tailwindClasses} flex w-[55px] h-[50px] 2xs:w-[65px] 2xs:h-[60px]
       sm:!w-[95px] sm:!h-[80px] flex-col justify-center transition-all duration-300 rounded-xl items-center p-1 text-lg sm:text-4xl border-2
       border-[#888] shadow-lg shadow-[#aaaaaa34] hover:scale-105 bg-gradient-to-tl from-[#424c5c] 
       to-[#171e2c]`
