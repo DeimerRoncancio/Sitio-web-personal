60847596f77de366a551437047162677fd62a6df
Deimer Roncancio <deimerroncancio@gmail.com>
2026-05-20 00:16:38 -0500

    feat: cambios en diseño responsive

--- src/dashboard/components/items-draggables.jsx
+++ src/dashboard/components/items-draggables.jsx
@@ -11,12 +11,12 @@ export default function ItemsDraggables({ icon, name, isDragging, isShadow = fal
     ${(isDragging && !isShadow) && 'scale-110 !h-[95px]'}`;
 
   return (
-    <div className={`draggable ${tailwindClasses} flex w-[95px] h-[80px]  flex-col justify-center
-      transition-all duration-300 rounded-xl items-center p-1 text-4xl border-2 border-[#888] shadow-lg
+    <div className={`draggable ${tailwindClasses} flex w-[65px] h-[60px] sm:w-[95px] sm:h-[80px]  flex-col justify-center
+      transition-all duration-300 rounded-xl items-center p-1 text-lg sm:text-4xl border-2 border-[#888] shadow-lg
       shadow-[#aaaaaa34] hover:scale-105 bg-gradient-to-tl from-[#424c5c] to-[#171e2c]`
     }>
       {icon}
-      <h3 className="text-sm text-white">{name}</h3>
+      <h3 className="text-[10px] leading-normal sm:text-sm text-white">{name}</h3>
     </div>
   )
 }
