a267e938d7289e74ea010cc8d974df2778ef051d
Deimer Roncancio <deimerroncancio@gmail.com>
2026-05-28 16:07:18 -0500

    style: pequeños detalles de estilo correjidos

--- src/dashboard/layouts/dashboard-layout.jsx
+++ src/dashboard/layouts/dashboard-layout.jsx
@@ -47,7 +47,7 @@ export default function DashboardLayout() {
         <DragDropProvider onDragEnd={(event) => changePage(event)} >
           <div style={{ display: currentView === 'dashboard' ? 'flex' : 'none' }}
           className="theme-sidebar bg-[rgb(23,30,44)] sm:flex-col justify-center sm:justify-normal w-full sm:w-auto
-          border-r border-[#2a3852] fixed bottom-0 sm:static order-2 sm:order-1 z-30">
+          border-r border-[#2a3852] fixed bottom-0 sm:static order-2 sm:order-1 z-40">
             <p className='theme-muted hidden sm:block text-[11px] transform scale-x-[1.1] text-center mt-4 text-[#979fa1]'>
               NAVEGACIÓN
             </p>
@@ -63,7 +63,7 @@ export default function DashboardLayout() {
               ))}
             </ul>
           </div>
-          <div className={`theme-dashboard dashboard bg-[#202a3e] w-full sm:!h-full relative order-1 sm:order-2
+          <div className={`theme-dashboard dashboard bg-[#202a3e] w-full sm:!h-full relative order-1 sm:order-2 z-50
           ${currentView === 'dashboard' ? 'h-[calc(100vh-135px)]' : 'h-full'}`}>
             <Droppable parent={parent} items={items}>
               <Outlet />
